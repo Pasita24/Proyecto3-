7ad7c99c27e35da5a1dc3c9982e899afeb2d327d
Pasita24 <benjapaz024@gmail.com>
2023-11-30 01:08:23 -0300

    Actualizacion

--- Proyecto3.cpp
+++ Proyecto3.cpp
@@ -1,9 +1,13 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <cstdlib>
 #include <iostream>
 #include <vector>
-#include <cstdlib>    // Para rand()
-#include <ctime>      // Para time()
-#include <algorithm>  // Para sort()
-
+#include <bits/stdc++.h>
+#include <random>
 using namespace std;
 
 void selectionSort(vector<int>& arr) {
@@ -20,7 +24,6 @@ void selectionSort(vector<int>& arr) {
         }
     }
 }
-
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
     bool swapped;
@@ -36,44 +39,138 @@ void bubbleSort(vector<int>& arr) {
     } while (swapped);
 }
 
-int main() {
-    srand(time(0)); // Inicializar la semilla para números aleatorios
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
 
-    int tamanoArreglo = rand() % 10001 + 100000; // Generar un tamaño aleatorio entre 100,000 y 110,000
-    vector<int> arregloAleatorio;
+void merge(vector<int>& arr, int start, int middle, int end, vector<int>& temp) {
+    int i = start;
+    int j = middle;
+    int k = start;
 
-    // Llenar el arreglo con números aleatorios sin repetición
-    for (int i = 0; i < tamanoArreglo; ++i) {
-        int numAleatorio = rand(); // Generar un número aleatorio
-        arregloAleatorio.push_back(numAleatorio);
+    for (int l = start; l < end; ++l) {
+        temp[l] = arr[l];
     }
 
-    vector<int> arregloCopia = arregloAleatorio; // Crear una copia del arreglo aleatorio para usar con Bubble Sort
+    while (i < middle && j < end) {
+        if (temp[i] <= temp[j]) {
+            arr[k] = temp[i];
+            ++i;
+        } else {
+            arr[k] = temp[j];
+            ++j;
+        }
+        ++k;
+    }
 
-    // Medir el tiempo antes de ordenar con Selection Sort
+    while (i < middle) {
+        arr[k] = temp[i];
+        ++i;
+        ++k;
+    }
+}
+
+void mergeSort(vector<int>& arr, int start, int end, vector<int>& temp) {
+    if (end - start <= 1) return;
+
+    int middle = start + (end - start) / 2;
+    mergeSort(arr, start, middle, temp);
+    mergeSort(arr, middle, end, temp);
+    merge(arr, start, middle, end, temp);
+}
+
+void partition(vector<int>& D, int low, int high) {
+    if (high <= low) return;
+
+    int i = low;
+    int j = high;
+    int s = D[low];
+
+    while (high > low) {
+        while (high > low && D[high] >= s) {
+            high--;
+        }
+        if (high > low) {
+            D[low] = D[high];
+            low++;
+        }
+        while (high > low && D[low] <= s) {
+            low++;
+        }
+        if (high > low) {
+            D[high] = D[low];
+            high--;
+        }
+    }
+    D[low] = s;
+    partition(D, i, low - 1);
+    partition(D, low + 1, j);
+}
+
+void quicksort(vector<int>& D) {
+    partition(D, 0, D.size() - 1);
+}
+
+int main() {
+    srand(time(0));
+
+    int tamanoArreglo = 20000; // Definir tamaño del arreglo
+
+    set<int> elementosAleatorios; // Usar un conjunto para evitar duplicados
+    vector<int> arregloAleatorio;
+    vector<int> arregloCopia;
+    vector<int> temp(tamanoArreglo);
+
+    while (elementosAleatorios.size() < tamanoArreglo) {
+        int numAleatorio = rand();
+        elementosAleatorios.insert(numAleatorio);
+    }
+
+    for (const auto& num : elementosAleatorios) {
+        arregloAleatorio.push_back(num);
+        arregloCopia.push_back(num);
+    }
+
     clock_t startSelection = clock();
-
-    // Ordenar el arreglo usando Selection Sort
     selectionSort(arregloAleatorio);
-
-    // Medir el tiempo después de ordenar con Selection Sort
     clock_t endSelection = clock();
     double tiempoSelection = double(endSelection - startSelection) / CLOCKS_PER_SEC;
 
-    // Medir el tiempo antes de ordenar con Bubble Sort
     clock_t startBubble = clock();
-
-    // Ordenar el arreglo copiado usando Bubble Sort
-    bubbleSort(arregloCopia);
-
-    // Medir el tiempo después de ordenar con Bubble Sort
+    bubbleSort(arregloAleatorio);
     clock_t endBubble = clock();
     double tiempoBubble = double(endBubble - startBubble) / CLOCKS_PER_SEC;
 
-    // Mostrar los tiempos de ejecución
+    clock_t startInsertion = clock();
+    insertionSort(arregloAleatorio);
+    clock_t endInsertion = clock();
+    double tiempoInsertion = double(endInsertion - startInsertion) / CLOCKS_PER_SEC;
+
+    clock_t startMerge = clock();
+    mergeSort(arregloAleatorio, 0, arregloAleatorio.size(), temp);
+    clock_t endMerge = clock();
+    double tiempoMerge = double(endMerge - startMerge) / CLOCKS_PER_SEC;
+
+    clock_t startQuick = clock();
+    quicksort(arregloAleatorio);
+    clock_t endQuick = clock();
+    double tiempoQuick = double(endQuick - startQuick) / CLOCKS_PER_SEC;
+
     cout << "Ordenamiento de forma aleatoria sin repeticiones:" << endl;
     cout << "Selection Sort: " << tiempoSelection << " segundos" << endl;
     cout << "Bubble Sort: " << tiempoBubble << " segundos" << endl;
+    cout << "Insertion Sort: " << tiempoInsertion << " segundos" << endl;
+    cout << "Merge Sort: " << tiempoMerge << " segundos" << endl;
+    cout << "Quick Sort: " << tiempoQuick << " segundos" << endl;
 
     return 0;
 }
